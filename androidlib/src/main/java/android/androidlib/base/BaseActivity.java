9097dbed767e3e06c5dfea571106256335208b1a
Liuml <951442843@qq.com>
2017-12-05 11:10:18 +0800

    Initial commit

---
+++ androidlib/src/main/java/android/androidlib/base/BaseActivity.java
@@ -0,0 +1,13 @@
+package android.androidlib.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author liuml
+ * @explain 与业务无关的 Activity 基类
+ * @time 2017/12/5 09:52
+ */
+
+public class BaseActivity extends AppCompatActivity{
+
+}
